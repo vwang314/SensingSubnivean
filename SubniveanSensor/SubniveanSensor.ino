ee45228f7d8655e22c4a8b050458d8cd26f9f427
vwang314 <vwang314@gmail.com>
2021-03-30 18:01:44 -0400

    update file to include both dht and waterproof temp sensor readings

--- SubniveanSensor/SubniveanSensor.ino
+++ SubniveanSensor/SubniveanSensor.ino
@@ -2,28 +2,40 @@
 #include <ArduinoECCX08.h>
 #include <ArduinoMqttClient.h>
 #include <WiFiNINA.h> // change to #include <WiFi101.h> for MKR1000
-
 #include "arduino_secrets.h"
-
 /////// Enter your sensitive data in arduino_secrets.h
 const char ssid[]        = SECRET_SSID;
 const char pass[]        = SECRET_PASS;
 const char broker[]      = SECRET_BROKER;
 const char* certificate  = SECRET_CERTIFICATE;
-
 WiFiClient    wifiClient;            // Used for the TCP socket connection
 BearSSLClient sslClient(wifiClient); // Used for SSL/TLS connection, integrates with ECC508
 MqttClient    mqttClient(sslClient);
-
 unsigned long lastMillis = 0;
 
-void setup() {
-    Serial.begin(115200);
-  while (!Serial);
 
+#include <DHT.h>
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature snowTemp(&oneWire);
+
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
   if (!ECCX08.begin()) {
-    Serial.println("No ECCX08 present!");
-    while (1);
+      Serial.println("No ECCX08 present!");
+      while (1);
   }
 
   ArduinoBearSSL.onGetTime(getTime);
@@ -32,6 +44,9 @@ void setup() {
   // Optional, set the client id used for MQTT,
   // mqttClient.setId("clientId");
   mqttClient.onMessage(onMessageReceived);
+
+  dht.begin();
+  snowTemp.begin();
 
 }
 
@@ -48,8 +63,18 @@ void loop() {
   // poll for new MQTT messages and send keep alives
   mqttClient.poll();
 
-
-  
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+  //Print temp and humidity values to serial monitor
+  Serial.print("Ambient Humidity: ");
+  Serial.print(hum);
+  Serial.print(" %, Ambient Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+  snowTemp.requestTemperatures();
+  Serial.print("Temperature is: ");
+  Serial.println(snowTemp.getTempCByIndex(0));
+  delay(1000); //Delay 2 sec.  
 }
 
 
